4adda037787ead014f588307cadf279d98ba6029
Rezi Gelenidze <rezi.gelenidze.05@gmail.com>
2021-06-28 23:24:11 +0400

    Add files via upload

---
+++ pset7/movies/12.sql
@@ -0,0 +1,13 @@
+/*In 12.sql, write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred.
+Your query should output a table with a single column for the title of each movie.
+You may assume that there is only one person in the database with the name Johnny Depp.
+You may assume that there is only one person in the database with the name Helena Bonham Carter.*/
+SELECT movies.title FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON movies.id = stars.movie_id
+    WHERE people.name = 'Johnny Depp' AND movies.title
+    IN(
+        SELECT movies.title FROM people
+            JOIN stars ON people.id = stars.person_id
+            JOIN movies ON movies.id = stars.movie_id
+            WHERE people.name = 'Helena Bonham Carter');
---
+++ pset9/finance/helpers.py
@@ -0,0 +1,88 @@
+import os
+import requests
+import urllib.parse
+import re
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+
+def check_pass(password):
+    pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}$"
+    result = re.findall(pattern, password)
+    if result:
+        return True
+    return False
+
+
+def remove_empty_stocks(data_list):
+    """ iterate over datalist and remove stock dictionary data with quantity of 0"""
+    indexes_of_empty = []
+
+    # iterate over data list to find stock with quantity 0, then append its
+    # index to indexes_of_empty list
+    for i in range(0, len(data_list)):
+        if data_list[i]['SUM(quantity)'] == 0:
+            indexes_of_empty.append(i)
+
+    # remove stocks with 0 quantity using their indexes from the last to first one
+    for index in indexes_of_empty[::-1]:
+        del data_list[index]
